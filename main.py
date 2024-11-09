d95b916bfdaab7dc0f42d81618ea130fa5c7b04d
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2024-11-09 22:35:50 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,371 @@
+import os, disnake, random, re
+import disnake.ext
+from disnake.ext import tasks
+from disnake.ext import commands
+from dotenv import load_dotenv
+from datetime import datetime,timedelta
+import riddle_of_theday
+import links 
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+ 
+intents = disnake.Intents.default()
+intents.message_content = True
+
+Bot = commands.Bot(
+    command_prefix=disnake.ext.commands.when_mentioned,
+    activity=disnake.Activity(type=disnake.ActivityType.watching, name="Geek Room The Anime"),
+    intents=intents
+)
+
+#ID of channel where questions are posted
+qotdtarget = int(os.getenv('QOTD_TARGET'))#1116378200096907264#fill in  #varxqotd 
+riddlechannel = int(os.getenv('RIDDLECHANNEL'))
+#ID of role that gets pinged when posting a question, leave blank string if no role should be pinged
+pingrole = os.getenv('PINGROLE') #Member role ID for @DSA  #varxmember
+
+
+#Hour QOTD is to be posted
+posttime = int(os.getenv('POSTTIME'))
+
+embedcolor = disnake.Colour.green() 
+errorembedcolor = disnake.Colour.red()
+
+#Scraping questions from leetecode
+import requests
+from bs4 import BeautifulSoup
+import random
+import re
+from disnake import utils
+async def question_post(channel):
+    #Scraping questions from leetecode
+    # Send a GET request to the website
+    r = requests.get('https://bishalsarang.github.io/Leetcode-Questions/out.html')
+
+    # Parse the HTML content
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    # Find all question titles and descriptions
+    questions = soup.find_all("div", class_="content__u3I1 question-content__JfgR")
+
+    # Select a random question
+    random_question = random.choice(questions)
+
+    # Extract question title with regex
+    title = random_question.find_previous_sibling("div", id="title").text.strip()
+    title = re.sub(r'^\d+.','',title)
+
+    # Extract question description and remove problematic Unicode characters
+    description = random_question.find("div").text.strip()
+    description_cleaned = description.replace('\u230a', '')  # Replace problematic Unicode character
+    #Prettify the text
+    description_cleaned = await riddle_of_theday.generate_response_with_text("Prettify the following description to be sent in a discord embed : " + description_cleaned)
+    
+    # Construct the problem link
+    title_with_hyphens = re.sub(r'[^\w\s-]', '', title).strip().replace(' ', '-').lower()
+    link_without_number = re.sub(r'^\d+-', '', title_with_hyphens)
+    link_without_number = "https://leetcode.com/problems/"+link_without_number
+
+    # Create an embed with the title, description, and link
+    embed = disnake.Embed(
+        title=title,
+        description=description_cleaned,
+        colour=embedcolor
+    )
+    embed.add_field(name="Problem Link", value=f"[Click here]({link_without_number})")
+    embed.set_author(name="Today's problem:", icon_url='https://images.playground.com/85f17db5dc3a4b38acc26419711b6c4d.jpeg')#replace with any other/bot icon
+    embed.set_footer(text=f"Daily Question #{datetime.now().date()}")
+
+    # Send the embed to the channel
+    guild = channel.guild  # Get the guild (server) of the channel
+    riddles_role = utils.get(guild.roles, name=pingrole)
+    if pingrole == "":
+        await channel.send(embed=embed)
+    else:
+        await channel.send(f"<@&{riddles_role.id}>", embed=embed)
+    message = channel.last_message
+    await message.create_thread(
+        name=f"'{title}'",
+        auto_archive_duration=60)
+
+    print(f'{Bot.user} has posted a qotd!')
+
+
+last_posted_date = None
+#Scheduled to call question of the day.
+
+
+@tasks.loop(hours = 1)
+async def task():
+    global last_posted_date
+    current_date = datetime.now().date()
+    if datetime.now().hour == posttime and last_posted_date!=current_date:
+        channel = Bot.get_channel(qotdtarget)
+        await question_post(channel) #daily DSA
+        last_posted_date = current_date
+        channel = Bot.get_channel(riddlechannel)
+        topics = ['Artificial Intelligence & Machine Learning','Cybersecurity','Robotics & IOT','DSA, Problem Solving and Aptitute']
+        for topic in topics:
+            await riddle_of_theday.question_post(channel,topic) #Riddle post
+        #question_post function posts DSA, whereas riddle_of_theday.question_post posts riddles
+            
+       
+
+#Make sure bot is online.
+@Bot.event
+async def on_ready():
+    print(f'{Bot.user} has connected to Discord! It is '+ str(datetime.now()))
+    await task.start()
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Reads for commands, which includes adding questions and testing
+        
+@Bot.slash_command(name="qotd", description="Sends QOTD for testing")
+@commands.has_permissions(administrator=True)
+async def send(inter: disnake.ApplicationCommandInteraction):
+    await inter.response.defer()
+   
+    try:
+        channel = Bot.get_channel(qotdtarget)
+        if channel is None :
+            raise ValueError(f"Channel with ID{qotdtarget} not found")
+        await question_post(channel)
+        
+        embed = disnake.Embed(
+            title = f"Success!",
+            description = f"Sent a QOTD.",
+            colour = embedcolor,
+        )
+        await inter.edit_original_message(embed=embed)
+    except Exception as e:
+        # Handle any errors and respond appropriately
+        error_embed = disnake.Embed(
+            title="Error!",
+            description=f"An error occurred: {str(e)}",
+            color=0xff0000,
+        )
+        await inter.edit_original_response(embed=error_embed)
+
+#riddle posting
+@Bot.slash_command(name="riddle", description="Sends daily riddle for testing")
+@commands.has_permissions(administrator=True)
+async def send(inter: disnake.ApplicationCommandInteraction,subject):
+    await inter.response.defer()
+    
+    try:
+        channel = inter.channel
+        if channel is None:
+            raise ValueError(f"Channel  not found")
+
+        # Assume riddle_of_theday.question_post(channel) is a function that posts the riddle
+        await riddle_of_theday.question_post(channel,subject)
+
+        embed = disnake.Embed(
+            title="Success!",
+            description="Sent a riddle.",
+            color=embedcolor,
+        )
+        
+        await inter.edit_original_response(embed=embed)
+    except Exception as e:
+        # Handle any errors and respond appropriately
+        error_embed = disnake.Embed(
+            title="Error!",
+            description=f"An error occurred: {str(e)}",
+            color=0xff0000,
+        )
+        await inter.edit_original_response(embed=error_embed)
+
+#==================================================================================================Links=======================================================================================
+#reduce redundancy with global_options
+global_options = links.global_options
+from links import handle_insta,handle_twitter,handle_whatsapp,handle_discord,handle_nas,handle_linkedin,handle_linktree
+# Event listener for select menu interactions
+@Bot.event
+async def on_dropdown(inter: disnake.MessageInteraction):
+    if inter.data.custom_id == "Bot-linkspanel":
+        # Get the selected value from the interaction
+        value = inter.values[0]
+        
+        # Call the appropriate handler function based on the selected value
+        if value == "twitter-linkspanel":
+            await handle_twitter(inter)
+        elif value == "insta-linkspanel":
+            await handle_insta(inter)
+        elif value == "whatsapp-linkspanel":
+            await handle_whatsapp(inter)
+        elif value == "discord-linkspanel":
+            await handle_discord(inter)
+        elif value == "nasio-linkspanel":
+            await handle_nas(inter)
+        elif value == "linkedin-linkspanel":
+            await handle_linkedin(inter)
+        elif value == "linktree-linkspanel":
+            await handle_linktree(inter)
+
+
+@Bot.slash_command(name="links", description="Get links to the Geek Room community")
+async def links(inter: disnake.ApplicationCommandInteraction):
+    # Create the select menu options
+    options = global_options
+
+    # Create the select menu
+    select_menu = disnake.ui.Select(
+        placeholder="❌┆Links",
+        options=options,
+        custom_id="Bot-linkspanel"
+    )
+
+    # Create an action row with the select menu
+    action_row = disnake.ui.ActionRow(select_menu)
+
+    # Create the embed
+    embed = disnake.Embed(
+        title="🔗・Links",
+        description="Get access to all Geek Room rescources from the menu!\nLinktree:",
+        color=0x3498db
+    )
+    embed.set_image(url="https://cdn.discordapp.com/attachments/843487478881976381/874694194474668052/Bot_banner_invite.jpg")
+
+    # Send the response with the embed and action row
+    await inter.response.send_message(embed=embed, components=[action_row])
+
+@Bot.slash_command(name="say", description="Send a message containing the value")
+@commands.has_permissions(administrator=True)  # Require admin permissions
+async def say(inter: disnake.ApplicationCommandInteraction, channel_id , message: str):
+    channel = Bot.get_channel(channel_id)
+    
+    # Check if the channel exists
+    if channel is None:
+        embed = disnake.Embed(
+            title="Error!",
+            description="The specified channel does not exist.",
+            color=0xff0000,
+        )
+        return await inter.send(embed=embed)
+    
+    # Send the message to the specified channel
+    await channel.send(message)
+    
+    embed = disnake.Embed(
+        title="Success!",
+        description=f"Sent the message in {channel.mention}.",
+        color=embedcolor,
+    )
+    await inter.send(embed=embed)
+
+# Error handling for users without permissions
+@say.error
+async def say_error(inter: disnake.ApplicationCommandInteraction, error):
+    if isinstance(error, commands.MissingPermissions):
+        embed = disnake.Embed(
+            title="Permission Denied!",
+            description="You do not have permission to use this command.",
+            color=0xff0000,
+        )
+        await inter.send(embed=embed)
+
+@Bot.slash_command(name="summarise", description="Summarise chat upto a week")
+@commands.has_permissions(administrator=True) 
+async def summarise(inter, channel_id):
+    await inter.response.defer()  # Defer the interaction first
+    
+    channel = Bot.get_channel(int(channel_id))
+    
+    # Calculate the start and end dates for the week
+    end_date = datetime.now()  # Current local date and time
+    start_date = end_date - timedelta(days=7)  # 7 days ago
+    last_week = True
+    summary = ''
+    # Fetch messages within the week
+    async for message in channel.history(limit=None, after=start_date, before=end_date):
+            print(message.content)
+            summary += f"{message.author.name}:{message.content}\n"
+    
+    if not summary:
+        last_week = False
+        print("No messages in the last 7 days, fetching the last 20 messages.")
+        summary = ''  # Reset summary
+        async for message in channel.history(limit=20):  # Limit set to 20
+            print(message.content)
+            summary += f"{message.author.name}: {message.content}\n"
+    
+    await riddle_of_theday.summarise(inter.channel,summary)
+    embed = disnake.Embed(
+        title = f"Success!",
+        description = f"Summarised text for the time period {start_date} to {end_date}" if last_week else f"Summarised the last 20 messages",
+        colour = embedcolor,
+    )
+    await inter.send(embed = embed)
+@summarise.error
+async def say_error(inter: disnake.ApplicationCommandInteraction, error):
+    if isinstance(error, commands.MissingPermissions):
+        embed = disnake.Embed(
+            title="Permission Denied!",
+            description="You do not have permission to use this command.",
+            color=0xff0000,
+        )
+        await inter.send(embed=embed)
+    
+Bot.run(TOKEN)
+
+
+#==========================================================================================================Embed Linkspanel alternative======================================================================================
+# @Bot.slash_command(name="link", description="Sends the Geek Room links")
+# async def link(inter: disnake.ApplicationCommandInteraction):
+#     await inter.response.defer()
+
+#     try:
+#         # Create an embed with the link
+#         embed = disnake.Embed(
+#             title="Links",
+#             description="Here are the links to Geek Room's resources",
+#             color=0x3498db
+#         )
+#         embed.add_field(
+#             name="Linktree",
+#             value="[Geek Room Linktree](https://linktr.ee/geekroom)",
+#             inline=False
+#         )
+#         embed.add_field(
+#             name="LinkedIn",
+#             value="[Official LinkedIn Page](https://www.linkedin.com/company/geekr00m/)",
+#             inline=False
+#         )
+#         embed.add_field(
+#             name="Instagram",
+#             value="[Official Instagram Page](https://www.instagram.com/geekr00m/)",
+#             inline=False
+#         )
+#         embed.add_field(
+#             name="Twitter",
+#             value="[Geek Room Twitter](https://twitter.com/geek__room_)",
+#             inline=False
+#         )
+#         embed.add_field(
+#             name="Whatsapp Group",
+#             value="[Geek Room Whatsapp](https://chat.whatsapp.com/EPDLVRHU1AM1HQ76NFWIDW)",
+#             inline=False
+#         )
+#         embed.add_field(
+#             name="Discord",
+#             value="[Official Discord](https://discord.com/invite/7TEVm4pmMv)",
+#             inline=False
+#         )
+#         embed.set_author(name=f"@geekroom",icon_url="https://ugc.production.linktr.ee/2a6c8d7a-a38a-45c4-9e9f-4a14b6c88714_GR-Logo.png?io=true&size=avatar-v3_0")
+#         # embed.set_image(url="https://ugc.production.linktr.ee/2a6c8d7a-a38a-45c4-9e9f-4a14b6c88714_GR-Logo.png?io=true&size=avatar-v3_0")
+#         embed.set_footer(text="Visit Geek Room for more awesome content!")
+
+#         # Send the embed
+#         await inter.edit_original_response(embed=embed)
+#     except Exception as e:
+#         # Handle any errors and respond appropriately
+#         error_embed = disnake.Embed(
+#             title="Error!",
+#             description=f"An error occurred: {str(e)}",
+#             color=0xff0000,
+#         )
+#         await inter.edit_original_response(embed=error_embed)
+
+#=================================================================================================================================================================================================
---
+++ riddle_of_theday.py
@@ -0,0 +1,205 @@
+import os, disnake, random, re
+import disnake.ext
+from discord.ext import commands
+import google.generativeai as genai
+from disnake.ext import tasks
+from disnake.ext import commands
+from dotenv import load_dotenv
+from datetime import datetime
+from pathlib import Path
+
+
+#Load Gemini Token and configure other variables
+load_dotenv()
+TOKEN = os.environ.get('DISCORD_TOKEN')
+GOOGLE_AI_KEY = os.environ.get('GOOGLE_AI_KEY')
+MAX_HISTORY  = 10
+message_history = {}
+
+# Configure the generative AI model-----------------------------------------------------------------------------------------------------------------------------------------------------------
+genai.configure(api_key=GOOGLE_AI_KEY)
+text_generation_config = {
+    "temperature": 0.2,
+    "top_p": 0.9,
+    "top_k": 10,
+    "max_output_tokens": 512,
+}
+image_generation_config = {
+    "temperature": 0.4,
+    "top_p": 1,
+    "top_k": 32,
+    "max_output_tokens": 512,
+}
+# safety_settings = [{
+#     "category": "HARM_CATEGORY_HARASSMENT",
+#     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+# }, {
+#     "category": "HARM_CATEGORY_HATE_SPEECH",
+#     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+# }, {
+#     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+#     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+# }, {
+#     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+#     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+# }]
+text_model = genai.GenerativeModel(model_name="gemini-pro",
+                                   generation_config=text_generation_config,
+                                   #safety_settings=safety_settings
+                                   )
+image_model = genai.GenerativeModel(model_name="gemini-pro-vision",
+                                    generation_config=image_generation_config,
+                                    #safety_settings=safety_settings
+                                    )
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+Bot = commands.Bot(
+    command_prefix=disnake.ext.commands.when_mentioned,
+    activity=disnake.Activity(type=disnake.ActivityType.watching, name="dogroom fetcher")
+)
+
+
+#ID of channel where questions are posted
+target = 1116378200096907264#fill in
+target2 = 1116378196678545430
+
+#ID of role that gets pinged when posting a question, leave blank string if no role should be pinged
+pingrole = "1116378165284196484"
+
+#Hour QOTD is to be posted
+posttime = 16#fill in
+
+embedcolor = disnake.Colour.green()
+
+errorembedcolor = disnake.Colour.red()
+
+#Question addition, not used for now, could be added later
+questionsfile = Path(__file__).with_name('questions.txt')
+linksfile = Path(__file__).with_name('questionlins.txt')
+tempfile = Path(__file__).with_name('temp.txt')
+
+#Adds a question to the text file when called.(Future functionality)
+def question_add(question):
+    with open(questionsfile, 'a') as questions:
+        questions.write(question+'\n')
+
+def remove_question(qotd):
+    with open (questionsfile, "r") as input:
+        with open (tempfile, "w") as output:
+            for line in input:
+                if line.strip("\n") != qotd:
+                    output.write(line)
+    os.replace(tempfile, questionsfile)
+progress = 0
+#===============================================================================Daily Riddle posting=======================================================================
+ 
+async def question_post(channel, subject):
+    """
+    Parameters:
+    - channel: The channel where the message will be sent.
+    - subject: The topic for the riddle (e.g., 'Web Development', 'Cybersecurity','AI).
+    """
+    # Validate the subject to ensure it's one of the allowed topics
+    # Sub commands/ options to choose from can be added later if required
+    # allowed_subjects = ['Web Development', 'Cybersecurity', 'DSA','AI','ML','Programming history','Robotics','IOT']
+    # if subject not in allowed_subjects:
+    #     raise ValueError("Invalid subject. Allowed subjects are: 'Web Development', 'Cybersecurity', 'DSA'.")
+
+    # Create the cleaned text based on the subject
+    cleaned_text = f"send a daily riddle on the following topic: {subject}. Do not give the answer, only the question. The title should be \"Daily riddle - {subject}\":"
+    
+    # Generate the response text
+    response_text = await generate_response_with_text(cleaned_text)
+    
+    # Add AI response to history
+    await split_and_send_messages(channel, response_text, 1700)
+
+    return
+#==========================================================================================================================================================================
+#================================================================================Chat summary function ===================================================================
+async def summarise(channel,message):
+      # Ignore messages sent by the bot
+    cleaned_text = "Summarise the following chat in about 500-1000 words and give details on who said what and respond with \"Chat Summary : \"+ (your response)  :" + message
+
+
+    response_text = await generate_response_with_text(cleaned_text)
+    #add AI response to history
+    await split_and_send_messages(channel, response_text, 1700)
+    return
+
+#=====================================================================================================================================================================
+async def generate_response_with_text(message_text):
+  prompt_parts = [message_text]
+  print("Got textPrompt: " + message_text)
+  response = text_model.generate_content(prompt_parts)
+  if (response._error):
+    return "❌" + str(response._error)
+  return response.text
+
+async def split_and_send_messages(channel, text, max_length):
+
+  # Split the string into parts
+  messages = []
+  for i in range(0, len(text), max_length):
+    sub_message = text[i:i + max_length]
+    messages.append(sub_message)
+
+  # Send each part as a separate message
+  for string in messages:
+    await channel.send(string)
+
+
+last_posted_date = None
+#Scheduled to call question of the day.
+
+ 
+#Example usage of daily riddle posting for different subjects
+@tasks.loop(hours = 1)
+async def task():
+    global last_posted_date
+    current_date = datetime.now().date()
+    if datetime.now().hour == posttime and last_posted_date!=current_date:
+        channel = Bot.get_channel(target2)
+        await question_post(channel,'AI')
+        await question_post(channel,'Programming history')
+        await question_post(channel,'Cybersecurity')
+        await question_post(channel,'Robotics')
+        await question_post(channel,'Web Development')
+        last_posted_date = current_date
+
+
+#Make sure bot is online.
+@Bot.event
+async def on_ready():
+    print(f'{Bot.user} has connected to Discord! It is '+ str(datetime.now()))
+    await task.start()
+
+#Reads for commands, which includes adding questions and testing
+     
+@Bot.slash_command(name="riddle", description="Sends riddle for testing")
+# @commands.has_permissions(send_messages=True)
+async def send(inter,subject):
+    channel = Bot.get_channel(target)
+    await question_post(channel,subject)
+    embed = disnake.Embed(
+        title = f"Success!",
+        description = f"Sent a daily riddle.",
+        colour = embedcolor,
+    )
+    await inter.send(embed=embed)
+
+#Add question, can be added later
+@Bot.slash_command(name="add", description="Add a QOTD to the list")
+async def add(inter, question):
+    embed = disnake.Embed(
+        title = f"Added QOTD! Thank you for submitting.",
+        description = f"'{question}'",
+        colour = embedcolor,
+    )
+    await inter.send(embed=embed)
+    question_add(question)
+    print(f'{inter.author} has added "{question}"!')
+
+if __name__ == '__main__':
+   Bot.run(TOKEN)
